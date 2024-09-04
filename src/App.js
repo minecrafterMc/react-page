452c6569aed4067257e3c759efa33d8dd61d0d0b
minecrafterMc <78699603+minecrafterMc@users.noreply.github.com>
2024-09-04 19:58:14 +0200

    Improved the popup menu

--- src/App.js
+++ src/App.js
@@ -1,27 +1,66 @@
-import './App.css';
-const data = {"tetis":{"Icon":"https://minecraftermc.github.io/react-page/assets/TetisIcon.png","name":"Tetis"}}
+import "./App.css";
+import data from "./data";
+import ReactDOMServer from "react-dom/server";
 
-function Icon({Id}){
-  function handleClick(){
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return width;
+}
+var popup;
+if (getWindowDimensions() > 600) {
+  popup = "popup";
+} else {
+  popup = "small-popup";
+}
+function Icon({ Id }) {
+  function handleClick() {
+    let patchList = data[Id].changelog.map((version) => (
+      <div key={version.id}>
+        <h2 className="popup-patchNotes-title">{version.number}</h2>
+        <ul>
+          {version.notes.map((ver) => (
+            <li key={ver.id}>{ver.text}</li>
+          ))}
+        </ul>
+      </div>
+    ));
     document.getElementById("popup-name").innerHTML = data[Id].name;
-    document.getElementById("popup").style.display = "block"
+    document.getElementById("popup-desc").innerHTML = data[Id].description;
+    document.getElementById("popup-img").src = data[Id].banner;
+    document.getElementById("popup-patchNotes").innerHTML =
+      ReactDOMServer.renderToString(patchList);
+    document.getElementById("popup").style.display = "block";
   }
-  return (
-    <img className='Icon' src={data[Id].Icon} onClick={handleClick} />
-  )
+  let iconname;
+  if (getWindowDimensions() < 600) {
+    iconname = "small-Icon";
+  } else {
+    iconname = "Icon";
+  }
+  return <img className={iconname} src={data[Id].Icon} onClick={handleClick} />;
 }
 
 function App() {
+  function closePopup() {
+    document.getElementById("popup").style.display = "none";
+  }
   return (
     <div className="App">
-      <div className='popup' id='popup'>
-        <h1 className='title' id='popup-name'></h1>
+      <div className={popup} id="popup">
+        <img id="popup-img" className="fade-img" />
+        <button className="popup-close" onClick={closePopup}>
+          close
+        </button>
+        <h1 className="popup-title" id="popup-name"></h1>
+        <h3 className="popup-title" id="popup-desc"></h3>
+        <h2 className="popup-title">Patch Notes:</h2>
+        <div id="popup-patchNotes"></div>
       </div>
-     <div className='Games'>
-      <h1 className='title'>Games</h1>
-      <Icon Id="tetis"></Icon>
-      <Icon Id="tetis"></Icon>
-      <Icon Id="tetis"></Icon>
+      <div className="Games">
+        <h1 className="title">Games</h1>
+        <Icon Id="tetis"></Icon>
+        <Icon Id="tetis"></Icon>
+        <Icon Id="tetis"></Icon>
       </div>
     </div>
   );

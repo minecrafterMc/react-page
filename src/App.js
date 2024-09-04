4e1c1dda334ae120ec1c8fd6fd4f1cc2be29768d
minecrafterMc <78699603+minecrafterMc@users.noreply.github.com>
2024-09-04 16:05:56 +0200

    configure react app for deployment to github pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+const data = {"tetis":{"Icon":"https://minecraftermc.github.io/react-page/assets/TetisIcon.png","name":"Tetis"}}
+
+function Icon({Id}){
+  function handleClick(){
+    document.getElementById("popup-name").innerHTML = data[Id].name;
+    document.getElementById("popup").style.display = "block"
+  }
+  return (
+    <img className='Icon' src={data[Id].Icon} onClick={handleClick} />
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='popup' id='popup'>
+        <h1 className='title' id='popup-name'></h1>
+      </div>
+     <div className='Games'>
+      <h1 className='title'>Games</h1>
+      <Icon Id="tetis"></Icon>
+      <Icon Id="tetis"></Icon>
+      <Icon Id="tetis"></Icon>
+      </div>
     </div>
   );
 }

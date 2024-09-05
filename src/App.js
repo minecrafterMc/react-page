792b3c6e583bcfa318b7a9bc9fc041a4617932ac
minecrafterMc <78699603+minecrafterMc@users.noreply.github.com>
2024-09-05 20:50:24 +0200

    further improved the popup menu

--- src/App.js
+++ src/App.js
@@ -2,10 +2,25 @@ import "./App.css";
 import data from "./data";
 import ReactDOMServer from "react-dom/server";
 
+
 function getWindowDimensions() {
   const { innerWidth: width, innerHeight: height } = window;
   return width;
 }
+let slideIndex = 0;
+var slideTimeout;
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("popup-gallery-image");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  slideTimeout = setTimeout(showSlides, 3000); // Change image every 2 seconds
+} 
 var popup;
 var gameDestination;
 if (getWindowDimensions() > 600) {
@@ -25,14 +40,27 @@ function Icon({ Id }) {
         </ul>
       </div>
     ));
+    let gallery = data[Id].gallery.map((image) => (
+      <div className="popup-gallery-image fade" key={image.id}>
+  <div className="popup-gallery-numbertext">{image.id} / {data[Id].gallery.length}</div>
+  <img src={image.url} style={{width: "100%"}} />
+  {image.caption != "" && <div className="popup-gallery-text">{image.caption}</div>}
+</div>
+     
+    ));
+    console.log(gallery)
     document.getElementById("popup-name").innerHTML = data[Id].name;
     document.getElementById("popup-desc").innerHTML = data[Id].description;
     document.getElementById("popup-img").src = data[Id].banner;
     gameDestination = data[Id].url;
     document.getElementById("popup-patchNotes").innerHTML =
       ReactDOMServer.renderToString(patchList);
+      document.getElementById("popup-gallery").innerHTML =
+      ReactDOMServer.renderToString(gallery);
     document.getElementById("popup-footer").innerHTML = data[Id].footer;
     document.getElementById("popup").style.display = "block";
+    clearTimeout(slideTimeout);
+    showSlides();
   }
   let iconname;
   if (getWindowDimensions() < 600) {
@@ -52,6 +80,9 @@ function PlayButton(){
     </div>
   )
 }
+function Gallery(){
+
+}
 function App() {
   function closePopup() {
     document.getElementById("popup").style.display = "none";
@@ -64,15 +95,24 @@ function App() {
           close
         </button>
         <h1 className="popup-title" id="popup-name"></h1>
+        
         <h3 className="popup-title" id="popup-desc"></h3>
         <PlayButton />
+        <h2 className="popup-title">Screenshots:</h2>
+        <div id="popup-gallery"></div>
         <h2 className="popup-title">Patch Notes:</h2>
+        
         <div id="popup-patchNotes"></div>
         <h3 className="popup-title" id="popup-footer"></h3>
       </div>
-      <div className="Games">
-        <h1 className="title">Games</h1>
+      <h1 className="title">Games</h1>
+      <div className="kontener">
         <Icon Id="tetis" />
+        <Icon Id="mineSlapper" />
+      </div>
+      <h1 className="title">Tools</h1>
+      <div className="kontener">
+
         <Icon Id="tetisEditor" />
       </div>
     </div>
--- src/data.js
+++ src/data.js
@@ -43,6 +43,12 @@ const data = {
           { text: "a ton of bugfixes", key: 11 }
         ]
       }
+    ],
+    gallery:[
+      { url: "https://minecraftermc.github.io/react-page/assets/screenshots/tetis/1.png", caption:"", id: 1},
+      { url: "https://minecraftermc.github.io/react-page/assets/screenshots/tetis/2.png", caption:"", id: 2},
+      { url: "https://minecraftermc.github.io/react-page/assets/screenshots/tetis/3.png", caption:"", id: 3},
+      { url: "https://minecraftermc.github.io/react-page/assets/screenshots/tetis/4.png", caption:"the quick mode maker", id: 4}
     ]
   },
   tetisEditor: {
@@ -51,44 +57,50 @@ const data = {
    banner: "https://minecraftermc.github.io/react-page/assets/tetisEditor.png",
    url:"https://minecraftermc.github.io/projectsFiles/tetis-editor/",
     description: "a mode editor for tetis",
-    footer:"<a href='https://github.com/minecraftermc/tetis'>github</a><br />Sfx credits: zapsplat.com<br />Made by MinecrafterMc",
+    footer:"<a href='https://github.com/minecraftermc/tetis-editor'>github</a><br />Made by MinecrafterMc, Krysoer",
     changelog: [
       {
         id: 1,
-        number: "2.2",
+        number: "1.0",
         notes: [
-          { text: "custom modes", key: 1 },
-          { text: "mode maker", key: 2 },
-          { text: "quick mode maker", key: 3 },
-          { text: "Added an option to export modes as files and URLs", key: 4 },
-          { text: "Added mods, which allow mode creators to change how the game functions by leting them execute custom code", key: 5 },
-          { text: "A lot of bugfixes", key: 6 }
+          { text: "released", key: 1 },
+          { text: "mode version is now 1.0", key: 2 }
+        ]
+      }
+    ],
+    gallery:[
+      { url: "https://minecraftermc.github.io/react-page/assets/screenshots/tetisEditor/1.png", caption:"", id: 1},
+      { url: "https://minecraftermc.github.io/react-page/assets/screenshots/tetisEditor/2.png", caption:"", id: 2},
+      { url: "https://minecraftermc.github.io/react-page/assets/screenshots/tetisEditor/3.png", caption:"", id: 3}
+    ]
+  },
+  mineSplapper: {
+    Icon: "https://minecraftermc.github.io/react-page/assets/MineSlapperIcon.png",
+    name: "Tetis",
+   banner: "https://minecraftermc.github.io/react-page/assets/mineSlapper.png",
+   url:"https://minecraftermc.github.io/projectsFiles/MineSlapper",
+    description: "Minesweeper, but made by me.",
+    footer:"<a href='https://github.com/minecrafterMc/Mineslapper'>github</a><br />Made by MinecrafterMc",
+    changelog: [
+      {
+        id: 1,
+        number: "1.1",
+        notes: [
+          { text: "bugfixes", key: 1 }
         ]
       },
       {
         id: 2,
-        number: "2.1",
+        number: "1.0",
         notes: [
-          { text: "added tournaments", key: 1 }
-        ]
-      },
-      {
-        id: 3,
-        number: "2.0",
-        notes: [
-          { text: "settings", key: 1 },
-          { text: "main menu", key: 2 },
-          { text: "color paletes", key: 3 },
-          { text: "new logo", key: 4 },
-          { text: "autoscaling", key: 5 },
-          { text: "loose screen", key: 6 },
-          { text: "multiple bonus modes", key: 7 },
-          { text: "next shape display", key: 8 },
-          { text: "sounds", key: 9 },
-          { text: "IOS support", key: 10 },
-          { text: "a ton of bugfixes", key: 11 }
+          { text: "release", key: 1 }
         ]
       }
+    ],
+    gallery:[
+      { url: "https://minecraftermc.github.io/react-page/assets/screenshots/mineSlapper/1.png", caption:"", id: 1},
+      { url: "https://minecraftermc.github.io/react-page/assets/screenshots/mineSlapper/2.png", caption:"", id: 2},
+      { url: "https://minecraftermc.github.io/react-page/assets/screenshots/mineSlapper/3.png", caption:"", id: 3}
     ]
   }
 }
